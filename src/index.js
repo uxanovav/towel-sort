6556a3ca39aa0482955bc1803021bcaf414b3f23
uxanovav <uxanovav@yandex.ru>
2020-10-23 11:28:19 +0400

    feats: task complited

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let resultArr = [];
+  let tempArr = [];
+  let flag = true;
+  if (typeof matrix !== "undefined") {
+    if (matrix.length !== 0) {
+      for (let i = 0; i < matrix.length; i++) {
+        if (flag === true) {
+          let swp = 0;
+          tempArr = matrix[i];
+          for (let j = 0; j < tempArr.length - 1; j++) {
+            for (let t = tempArr.length - 1; t > j; t--) {
+              if (tempArr[j] > tempArr[t]) {
+                swp = tempArr[t];
+                tempArr[t] = tempArr[j];
+                tempArr[j] = swp;
+              }
+            }
+          }
+          for (let el of tempArr) {
+            resultArr.push(el);
+          }
+          flag = !flag;
+        } else {
+          let swp = 0;
+          tempArr = matrix[i];
+          for (let j = 0; j < tempArr.length - 1; j++) {
+            for (let t = tempArr.length - 1; t > j; t--) {
+              if (tempArr[j] < tempArr[t]) {
+                swp = tempArr[t];
+                tempArr[t] = tempArr[j];
+                tempArr[j] = swp;
+              }
+            }
+          }
+          for (let el of tempArr) {
+            resultArr.push(el);
+          }
+          flag = !flag;
+        }
+      }
+    }
+  }
+  return resultArr;
 }
